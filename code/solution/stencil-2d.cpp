ae10e434967f1599b81530fbf802a3cb55c28eef
Sebastian Kuckuk <sebastian.kuckuk@fau.de>
2026-02-23 12:17:05 +0100

    add exercise solutions

---
+++ code/solution/stencil-2d.cpp
@@ -0,0 +1,90 @@
+#include <chrono>
+#include <cmath>
+#include <iostream>
+
+
+int main(int argc, char *argv[]) {
+    // default parameters
+    size_t nx = 4096;  // number of points in x direction
+    size_t ny = 4096;  // number of points in y direction
+    size_t nIt = 64;   // number of iterations to be performed
+
+    // override with command line arguments
+    int i = 1;
+    if (argc > i) nx = atoi(argv[i]);
+    ++i;
+    if (argc > i) ny = atoi(argv[i]);
+    ++i;
+    if (argc > i) nIt = atoi(argv[i]);
+    ++i;
+    
+    // allocate data
+    auto u = new double[nx * ny];
+    auto uNew = new double[nx * ny];
+
+    #pragma omp target enter data map(alloc: u[0:nx*ny], uNew[0:nx*ny])
+
+    // init fields - 0 on the inside, 1 at the boundaries
+    #pragma omp target loop collapse(2)
+    for (size_t j = 0; j < ny; ++j) {
+        for (size_t i = 0; i < nx; ++i) {
+            if (0 == i || 0 == j || nx - 1 == i || ny - 1 == j) {
+                u[j * nx + i] = 0;
+                uNew[j * nx + i] = 0;
+            } else {
+                u[j * nx + i] = 1;
+                uNew[j * nx + i] = 1;
+            }
+        }
+    }
+
+    // start measurement
+    auto start = std::chrono::steady_clock::now();
+
+    // perform nIt iterations
+    for (size_t i = 0; i < nIt; ++i) {
+        // update all inner points
+        #pragma omp target loop collapse(2)
+        for (size_t j = 1; j < ny - 1; ++j) {
+            for (size_t i = 1; i < nx - 1; ++i) {
+                uNew[j * nx + i] = 0.25 * (  u[j * nx + i - 1] \
+                                           + u[j * nx + i + 1] \
+                                           + u[(j - 1) * nx + i] \
+                                           + u[(j + 1) * nx + i]);
+            }
+        }
+
+        // swap source and destination array
+        std::swap(u, uNew);
+    }
+
+    // finish measurement
+    auto end = std::chrono::steady_clock::now();
+    auto elapsedSeconds = std::chrono::duration<double>(end - start);
+
+    std::cout << "  #cells / #it:  " << nx * ny << " / " << nIt << "\n";
+    std::cout << "  elapsed time:  " << 1e3 * elapsedSeconds.count() << " ms\n";
+    std::cout << "  per iteration: " << 1e3 * elapsedSeconds.count() / nIt << " ms\n";
+    std::cout << "  MLUP/s:        " << 1e-6 * nx * ny * nIt / elapsedSeconds.count() << "\n";
+
+    // check solution - compute the final residual
+    double res = 0;
+    #pragma omp target loop collapse(2) reduction(+ : res)
+    for (size_t j = 1; j < ny - 1; ++j) {
+        for (size_t i = 1; i < nx - 1; ++i) {
+            const double localRes = 4 * u[j * nx + i] - (u[j * nx + i - 1] + u[j * nx + i + 1] + u[(j - 1) * nx + i] + u[(j + 1) * nx + i]);
+            res += localRes * localRes;
+        }
+    }
+
+    #pragma omp target exit data map(release: u[0:nx*ny], uNew[0:nx*ny])
+
+    res = sqrt(res);
+    std::cout << "  Final residual is " << res << std::endl;
+
+    // clean up
+    delete[] u;
+    delete[] uNew;
+
+    return 0;
+}
---
+++ code/solution/stream-target-data.cpp
@@ -0,0 +1,91 @@
+#include <chrono>
+#include <cmath>
+#include <iostream>
+
+
+int main(int argc, char *argv[]) {
+    // default parameters
+    size_t nx = 64*1024*1024;  // number of points
+    size_t nIt = 4;            // number of iterations to be performed
+
+    // override with command line arguments
+    int i = 1;
+    if (argc > i) nx = atoi(argv[i]);
+    ++i;
+    if (argc > i) nIt = atoi(argv[i]);
+    ++i;
+    
+    // allocate data
+    auto src = new double[nx];
+    auto dest = new double[nx];
+
+    // init array elements with their index values
+    for (size_t i = 0; i < nx; ++i) {
+        src[i] = i;
+        dest[i] = i;
+    }
+
+    // start measurement
+    auto start = std::chrono::steady_clock::now();
+
+    #pragma omp target enter data map(to: src[0:nx]) map(alloc: dest[0:nx])
+
+    // perform nIt iterations
+    for (size_t i = 0; i < nIt; ++i) {
+        std::cout << "Iteration " << i << std::endl;
+
+        // start iteration measurement
+        auto startIt = std::chrono::steady_clock::now();
+
+        // copy array elements and increase each by one
+        #pragma omp target \
+                teams distribute \
+                parallel for \
+                num_teams(84 * 8 * 8) \
+                num_threads(256) \
+                map(to: src[0:nx]) \
+                map(from: dest[0:nx])
+        for (size_t i = 0; i < nx; ++i)
+            dest[i] = src[i] + 1;
+
+        // swap source and destination array
+        std::swap(src, dest);
+
+        // finish measurement
+        auto endIt = std::chrono::steady_clock::now();
+        auto elapsedSecondsIt = std::chrono::duration<double>(endIt - startIt);
+
+        std::cout << "  elapsed time:  " << 1e3 * elapsedSecondsIt.count() << " ms\n";
+        std::cout << "  bandwidth:     " << 1e-9 * 2 * sizeof(double) * nx / elapsedSecondsIt.count() << " GB/s\n";
+        std::cout << std::endl;
+    }
+
+    #pragma omp target exit data map(from: src[0:nx]) map(release: dest[0:nx])
+
+    // finish measurement
+    auto end = std::chrono::steady_clock::now();
+    auto elapsedSeconds = std::chrono::duration<double>(end - start);
+
+    std::cout << "Accumulated" << std::endl;
+    std::cout << "  #cells / #it:  " << nx << " / " << nIt << "\n";
+    std::cout << "  elapsed time:  " << 1e3 * elapsedSeconds.count() << " ms\n";
+    std::cout << "  per iteration: " << 1e3 * elapsedSeconds.count() / nIt << " ms\n";
+    std::cout << "  MLUP/s:        " << 1e-6 * nx * nIt / elapsedSeconds.count() << "\n";
+    std::cout << "  bandwidth:     " << 1e-9 * 2 * sizeof(double) * nx * nIt / elapsedSeconds.count() << " GB/s\n";
+
+    // check solution - each element should be increased by 
+    for (size_t i = 0; i < nx; ++i) {
+        if (i + nIt != src[i]) {
+            std::cout << "  Stream check failed for element " << i << " (expected " << i + nIt << " but got " << src[i] << ")" << std::endl;
+            break;
+        }
+    }
+
+    std::cout << "  Stream check completed" << std::endl;
+
+    // clean up
+    delete[] src;
+    delete[] dest;
+
+    return 0;
+}
